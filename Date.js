145899f422274712e199f9b44c6a4ae6d1c52997
Jacob <jacobharv00@gmail.com>
2021-03-17 18:05:43 -0600

    updated notes

---
+++ Date.js
@@ -0,0 +1,32 @@
+
+// const now = new Date()
+// const timestamp = now.getTime()
+
+// const myDate = new Date(timestamp)
+// console.log(myDate.getFullYear())
+
+// const dateOne = new Date('March 10th 2020 04:20:00')
+// const dateTwo = new Date()
+// const dateOneTimestamp = dateOne.getTime()
+// const dateTwoTimestamp = dateTwo.getTime()
+
+// if (dateOneTimestamp < dateTwoTimestamp) {
+//     console.log(dateOne.toString())
+// } else if (dateTwoTimestamp < dateOneTimestamp) {
+//     console.log(dateTwo.toString())
+// }
+
+
+
+
+// Unix Epoch - January 1st 1970 00:00:00
+// console.log(now.toString()) returns in a string form from the Date() call
+// console.log(now.getTime()) gets a number in miliseconds from unix epoch to today
+
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
